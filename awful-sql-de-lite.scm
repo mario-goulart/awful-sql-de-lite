a3a81568f4d5303c16572e8feb100873c63f3208
Mario Domenech Goulart <mario.goulart@gmail.com>
2010-06-03 13:16:02 +0000

    awful-sql-de-lite (trunk): `db-inquirer' supports the `value' keyword parameter (patch by Stephen Eilert)

--- awful-sql-de-lite.scm
+++ awful-sql-de-lite.scm
@@ -11,9 +11,14 @@
 
   (db-disconnect close-database)
 
-  (db-inquirer (lambda (q #!key default)
-                 (query (map-rows (lambda (data) data))
-                        (sql (db-connection) q))))
+  (db-inquirer (lambda (q #!key default values)
+                 (if values
+                     (apply query (append (list
+                                         (map-rows (lambda (data) data))
+                                         (sql (db-connection) q))
+                                        values))
+                     (query (map-rows (lambda (data) data))
+                            (sql (db-connection) q)))))
 
   (sql-quoter (lambda (data)
                 (++ "'" (string-substitute* (concat data) '(("'" . "''"))) "'")))
