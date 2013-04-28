ec33060ed3fd97d300866a46a1e73e58aeee7dc9
Mario Domenech Goulart <mario.goulart@gmail.com>
2013-04-27 21:18:52 -0300

    Indentation fixes

--- awful-sql-de-lite.scm
+++ awful-sql-de-lite.scm
@@ -17,9 +17,9 @@
   (db-inquirer (lambda (q #!key default values)
                  (if values
                      (apply query (append (list
-                                         (map-rows (lambda (data) data))
-                                         (sql (db-connection) q))
-                                        values))
+                                           (map-rows (lambda (data) data))
+                                           (sql (db-connection) q))
+                                          values))
                      (query (map-rows (lambda (data) data))
                             (sql (db-connection) q)))))
 
